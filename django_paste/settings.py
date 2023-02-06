7881f2c881535f4e08d7f6cc4dad751448bf71b3
Scynes <dustin.greyfield@gmail.com>
2023-02-06 13:56:23 -0500

    refactor: link -> links / paste -> pastes

--- django_paste/settings.py
+++ django_paste/settings.py
@@ -31,8 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paste',
-    'link'
+    'pastes',
+    'links'
 ]
 
 MIDDLEWARE = [
--- links/apps.py
+++ links/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LinkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'link'
+    name = 'links'
