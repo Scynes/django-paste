090bfafd52e447ae2662c2fbd599eb8cdad1f84e
Scynes <dustin.greyfield@gmail.com>
2023-02-06 11:36:37 -0500

    wip: Paste model created

--- paste/models.py
+++ paste/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import user
 
-# Create your models here.
+class Paste (models.Model):
+    
+    # Define the types of syntax
+    TEXT_SYNTAX = 'TXT'
+    JAVASCRIPT_SYNTAX = 'JS'
+    HTML_SYNTAX = 'HTML'
+
+    # Define the touple syntax
+    SYNTAX_OPTIONS = [
+        (TEXT_SYNTAX, 'Text'),
+        (JAVASCRIPT_SYNTAX, 'JavaScript'),
+        (HTML_SYNTAX, 'HTML')
+    ]
+
+    # Model fields
+    title = models.CharField(max_length=255)
+    syntax = models.CharField(max_length=4, choices=SYNTAX_OPTIONS, default=TEXT_SYNTAX)
+    private = models.BooleanField(default=False)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_created=True)
