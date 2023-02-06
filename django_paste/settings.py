5d6596fd6dd1d3980dd7e4c02257d783e31fcb98
Scynes <dustin.greyfield@gmail.com>
2023-02-06 13:15:50 -0500

    fix: Link -> GenericForeignKey arguments

--- django_paste/settings.py
+++ django_paste/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'paste',
+    'link'
 ]
 
 MIDDLEWARE = [
--- link/models.py
+++ link/models.py
@@ -5,4 +5,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Link (models.Model):
 
+    # Model fields
+    short_uuid = models.CharField(max_length=8)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    created_at = models.DateTimeField(auto_created=True)
