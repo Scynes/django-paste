eb3a307df2c841789436d5a543f4ba5919b27835
Scynes <dustin.greyfield@gmail.com>
2023-02-07 12:06:58 -0500

    wip: added pastes `upload` base template

--- pastes/urls.py
+++ pastes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.show_paste, name="pastes")
+    path('', views.PasteUploadView.as_view())
 ]
--- pastes/views.py
+++ pastes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class PasteUploadView(View):
+    
+    def get(self, request):
+        
+        return render(request, 'paste_upload.html')
