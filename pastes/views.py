e0ac08ab300eb796cce8c62619c784662ea69c66
Scynes <dustin.greyfield@gmail.com>
2023-02-07 14:05:10 -0500

    wip: paste submits to database, validation. removed sqlite db from git tracking

--- pastes/views.py
+++ pastes/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import View
 
@@ -10,5 +12,25 @@ class PasteUploadView(View):
         context = { 'form': PasteForm() }
         
         return render(request, 'paste_upload.html', context)
+
+    def post(self, request):
+        
+        form_data = PasteForm(request.POST)
+
+        if not form_data.is_valid():
+            return HttpResponse('Not valid hoe')
+
+        # Validation good, let's get a record going
+        paste = form_data.save(commit=False)
+
+        # Check if there's a user associated
+        if isinstance(request.user, User):
+            
+            paste.user = request.user
+
+        # Save the paste
+        paste.save()
+        
+        return HttpResponse(paste)
 
 
