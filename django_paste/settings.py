6c6e2440fba1c79498bdeb9ec3fb8b17af68bac4
Scynes <dustin.greyfield@gmail.com>
2023-02-07 12:07:18 -0500

    complete: added default GET CBV route for pastes

--- django_paste/settings.py
+++ django_paste/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'django_paste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
