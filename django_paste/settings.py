87c576869aca22a112c9d7bbc8643c23c5ae7f84
Scynes <dustin.greyfield@gmail.com>
2023-02-13 02:44:41 -0500

    complete: setup remote postgresql

--- django_paste/settings.py
+++ django_paste/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'django_paste.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT')
     }
 }
 
